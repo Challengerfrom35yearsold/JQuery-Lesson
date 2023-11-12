94e2e9f65d3c5beb9af1e9748652f7ca7b7f3845
Challengerfrom35yearsold <124878663+Challengerfrom35yearsold@users.noreply.github.com>
2023-11-12 05:10:00 +0000

    [Finish]JavaScriptを学ぼう,7章 【thisとchildren】

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
+// $(function(){
+//   $('.bg1').on('click', function(){
+//     $('.bg1').slideUp();
+//   });
+
+//   $('.bg2').on('click', function(){
+//     $('.bg2').slideUp();
+//   });
+
+//   $('.bg3').on('click', function(){
+//     $('.bg3').slideUp();
+//   });
+
+//   $('.bg4').on('click', function(){
+//     $('.bg4').slideUp();
+//   });
+// });
+
 $(function(){
   $('.box1').on('click', function(){
-    $('.box1').addClass('box1-ext');
+    $(this).slideUp();
   });
-  $('.box1').mouseout(function(){
-    $('.box1').removeClass('box1-ext');
+});
+
+$(function(){
+  $('button').on('click', function(){
+    $('ul').children().css('color','red');
   });
 });
